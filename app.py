20be75ef2797c603af3398556f7ea7bd42748a11
Mohammad-juned-khan <mohammad.juned.z.khan@gmail.com>
2021-03-08 22:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
+import en_core_web_sm
 
 from heapq import nlargest
 punctuation=punctuation+"\n"+" "+"  "
@@ -34,7 +35,8 @@ def sumy_summarize(docx):
 
 #function for spacy summary
 def spacy_process(text):
-    nlp = spacy.load('en_core_web_sm')
+    nlp = en_core_web_sm.load() #changed for 8 march
+    #nlp = spacy.load('en_core_web_sm')
     doc = nlp(text)
     #stopword = list(STOP_WORDS)
 
